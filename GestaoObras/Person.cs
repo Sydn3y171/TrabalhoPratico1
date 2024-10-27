ad03f8bf9d256fef91515e7995ef7b4ef0cdf79d
Mafalda <84814390+Sydn3y171@users.noreply.github.com>
2024-10-27 14:45:19 +0000

    Construtor and properties of person

--- GestaoObras/Person.cs
+++ GestaoObras/Person.cs
@@ -1,14 +1,56 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.AccessControl;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace GestaoObras
 {
-    internal class Person
+    public class Person
     {
+        #region Privete Properties
+        private int nif { get; set; }
+        private string endereco { get; set; }
+        private int telefone { get; set; }
+        #endregion
 
+        #region Public Properties
+        public string Nome
+        {
+            get
+            {
+                return this.Nome;
+            }
+            set
+            {
+                if (value.Length > 0)
+                {
+                    this.Nome = value;
+                }
+            }
+        }
+        public string Email
+        {
+            get
+            {
+                return this.Email;
+            }
+            set
+            {
+                this.Email = value;
+            }
+        }
 
+        #endregion
+
+        #region Construtor
+
+        public Person(string Name, string Email)
+        {
+            this.Nome = Nome;
+            this.Email = Email;
+        }
+        #endregion
     }
 }
