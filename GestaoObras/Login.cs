b2b658740eb3f92aa0d551139a38c94ffe2b0679
Mafalda <84814390+Sydn3y171@users.noreply.github.com>
2024-12-11 21:42:05 +0000

    update

--- GestaoObras/Login.cs
+++ GestaoObras/Login.cs
@@ -127,15 +127,15 @@ namespace GestaoObras
 
         #endregion
 
-        private void Password_KeyDown(object sender, KeyEventArgs e)
-        {
-            // Verifica se a tecla pressionada é a tecla Enter
-            if (e.KeyCode == Keys.Enter)
-            {
-                // Simula o clique do botão Login
-                Loginbutton.PerformClick();
-            }
-        }
+        //private void Password_KeyDown(object sender, KeyEventArgs e)
+        //{
+        //    // Verifica se a tecla pressionada é a tecla Enter
+        //    if (e.KeyCode == Keys.Enter)
+        //    {
+        //        // Simula o clique do botão Login
+        //        Loginbutton.PerformClick();
+        //    }
+        //}
 
         private void Registerbutton_Click(object sender, EventArgs e)
         {
--- GestaoObras/obj/Debug/net8.0-windows/GestaoObras.AssemblyInfo.cs
+++ GestaoObras/obj/Debug/net8.0-windows/GestaoObras.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestaoObras")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e6c0e223777d9a76b99bae6cf0f9ffdf6e673438")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ddbf554f53cdb6ef16cae3bf8784a8864c13bb98")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestaoObras")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestaoObras")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
